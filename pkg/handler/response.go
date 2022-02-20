78e501e9a470501c40d3f343219c3ff24946ba29
Goga Eva <igorevtushenko22@gmail.com>
2022-02-20 17:17:57 +0200

    add token parsing

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -6,11 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type error struct {
+type errorResponse struct {
 	Message string `json:"message"`
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	log.Print(message)
-	c.AbortWithStatusJSON(statusCode, error{message})
+	c.AbortWithStatusJSON(statusCode, errorResponse{message})
 }
